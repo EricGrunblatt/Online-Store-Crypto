9ee95f9d448c6a0bb19ff201149e4a511773afe4
Eric Chiu <echiu12@gmail.com>
2022-03-20 18:46:46 -0400

    implemented getProfileByUsername

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,11 +5,48 @@ const Image = require('../models/imageModel')
 const fs = require('fs')
 const path = require('path')
 const { createAndSaveImage, upload } = require('../handlers/imageHandler')
+const Products = require('../models/productModel')
+const Review = require('../models/reviewModel')
 
-// TODO
 getProfileByUsername = async (req, res) => {
 	console.log("getProfile", req.body)
+	
 	const {username} = req.body
+
+	let user = null
+	let json = {}
+	try {
+		if (!username) {
+			json = {status: constants.status.ERROR, errorMessage: constants.user.missingRequiredField}
+		}
+		else if (! (user = await User.findOne({username: username}))) {
+			json = {status: constants.status.ERROR, errorMessage: constants.user.userDoesNotExist}
+		}
+		else {
+			const profileImage = await Image.findById(user.profileImageId)
+
+			const sellingProducts = await Products.find({sellerUsername: username, buyerUsername: null})
+				.select({"_id": 1, "name": 1, "price": 1, "sellerUsername": 1, "dateListed": 1})
+
+			const reviews = await Review.find({forUsername: username})
+				.select({"_id": 0, "byUsername": 1, "stars": 1, "comment": 1})
+
+			json = {
+				status: constants.status.OK,
+				username: user.username,
+				profileImage: profileImage,
+				sellingProducts: sellingProducts,
+				reviews: reviews,
+				dateJoined: user.createdAt
+			}
+		}
+		console.log("RESPONSE:", json)
+		res.status(200).json(json)
+	}
+	catch (err) {
+		console.log(err)
+		res.status(500).send(constants.status.FATAL_ERROR)
+	}
 }
 
 // TODO
